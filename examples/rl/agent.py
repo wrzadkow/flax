1c483f325953b4b0fc5a7636cc94427db63f1bdc
Wojciech Rzadkowski <wojciech.rzadkowski@gmail.com>
2020-08-27 09:43:45 +0000

    Add device_get() (consolidates smaller ops into a bigger one)

--- examples/rl/agent.py
+++ examples/rl/agent.py
@@ -7,7 +7,7 @@ import math
 def greedy_action(model, state):
   out = model(state)
   action = jnp.argmax(out, axis = 1).astype(jnp.int32)
-  return action, out
+  return action
 
 def eps_greedy_action(state, optimizer, 
                       steps_done, eps_start, eps_end, eps_decay):
@@ -19,7 +19,8 @@ def eps_greedy_action(state, optimizer,
     rand_actions = onp.random.randint(low=0, high=4, size=(batch_size, ))
     return rand_actions
   else:
-    greedy_actions, out = greedy_action(optimizer.target, state)
+    greedy_actions = greedy_action(optimizer.target, state)
+    greedy_actions = jax.device_get(greedy_actions)
     return greedy_actions
     # return greedy_actions.block_until_ready()
   # r = onp.random.uniform(size=(batch_size, ))
